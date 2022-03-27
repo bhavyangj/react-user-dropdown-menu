5c71214809449e82fe38ab388d3706e3bbbf993d
Hassan Abdellah <hassanabdellah25@gmail.com>
2022-03-27 13:58:55 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/navbar/Navbar';
 
-function App() {
+const App = () => {
+  const user ={
+    name:'John Doe',
+    email:'johndoe@gmail.com',
+    image:'/images/user.jpg'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Navbar user={user}/>
     </div>
-  );
+  )
 }
 
 export default App;
